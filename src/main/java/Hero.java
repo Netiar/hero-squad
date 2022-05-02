96ab6564fa03c2d0077939bdde3075fe1037dba9
Raindrop <collins.odinga@student.moringaschool.com>
2022-05-03 01:38:56 +0300

    feat: added getters to hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String hName;
+    private int hPower;
+    private int hAge;
+    private int hWeakness;
+    private int hId;
+    public static List<Hero> instances = new ArrayList<Hero>();
+
+
+
+
 
 }
