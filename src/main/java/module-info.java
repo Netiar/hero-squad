763f20bca6d1a26e383f3574e37066638d22e609
Raindrop <collins.odinga@student.moringaschool.com>
2022-05-02 16:38:10 +0300

    deleted files

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.herosquad.herosquad {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.herosquad.herosquad to javafx.fxml;
-    exports com.herosquad.herosquad;
-}
